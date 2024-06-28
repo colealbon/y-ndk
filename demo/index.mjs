7355cc9892982067d4fc4b025da53190bd397fc6
Cole Albon <cole@cafe-society.news>
2024-06-28 10:15:41 -0700

    rename chat to demo because it is not a real chat

---
+++ demo/index.mjs
@@ -0,0 +1,15 @@
+import NDK, {
+  NDKPrivateKeySigner
+} from '@nostr-dev-kit/ndk'
+
+import {
+  createNostrCRDTRoom,
+  NostrProvider
+} from './y-ndk.mjs'
+
+export {
+  NDK,
+  NDKPrivateKeySigner,
+  createNostrCRDTRoom,
+  NostrProvider
+}
